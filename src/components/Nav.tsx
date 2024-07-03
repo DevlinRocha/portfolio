aeee415d41c7a0afdcc8a3cdca878f67b2eda70a
Devlin <devlincodes@gmail.com>
2024-07-02 19:55:19 -0700

    update nav styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,17 +2,17 @@ import { Link } from '@tanstack/react-router'
 
 function Nav() {
     return (
-        <>
-            <div className="flex gap-2 p-2">
+        <nav className="sticky top-0 z-10 bg-white">
+            <div className="flex gap-2 p-4">
                 <Link to="/" className="[&.active]:font-bold">
-                    Home
+                    home
                 </Link>{' '}
                 <Link to="/about" className="[&.active]:font-bold">
-                    About
+                    about
                 </Link>
             </div>
             <hr />
-        </>
+        </nav>
     )
 }
 
