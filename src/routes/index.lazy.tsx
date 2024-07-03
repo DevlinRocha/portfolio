828f912d2051713061a3fc9fc91496912a9eef15
Devlin <devlincodes@gmail.com>
2024-07-02 19:55:00 -0700

    add projects to index

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import Project from '../components/Project'
 
 export const Route = createLazyFileRoute('/')({
     component: Index,
@@ -6,14 +7,17 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
     return (
-        <div className="flex p-16">
-            <div>
-                <h1>Devlin Rocha</h1>
-                <h2 className="text-xs">Software Engineer</h2>
-                <span className="text-5xl">
-                    Hey there! 👋 I&apos;m Devlin, a frontend web developer in
-                    the San Francisco Bay Area!
-                </span>
+        <div className="flex h-full w-full flex-col gap-4 p-16">
+            <div className="flex flex-col text-4xl">
+                <span>hey there! 👋 i&apos;m Devlin, </span>
+                <span>a software engineer in the San Francisco Bay Area</span>
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+                <Project name="Banter" />
+                <Project name="VVordle" />
+                <Project name="Pokemon Roulette" />
+                <Project name="Where's Waldo?" />
             </div>
         </div>
     )
