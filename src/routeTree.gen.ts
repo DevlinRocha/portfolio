d585b73c9ce9a4476ef4a2323220fa63339a8947
Devlin <devlincodes@gmail.com>
2024-07-03 01:26:33 -0700

    run Prettier

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -23,54 +23,54 @@ const IndexLazyImport = createFileRoute('/')()
 // Create/Update Routes
 
 const BanterLazyRoute = BanterLazyImport.update({
-  path: '/banter',
-  getParentRoute: () => rootRoute,
+    path: '/banter',
+    getParentRoute: () => rootRoute,
 } as any).lazy(() => import('./routes/banter.lazy').then((d) => d.Route))
 
 const AboutLazyRoute = AboutLazyImport.update({
-  path: '/about',
-  getParentRoute: () => rootRoute,
+    path: '/about',
+    getParentRoute: () => rootRoute,
 } as any).lazy(() => import('./routes/about.lazy').then((d) => d.Route))
 
 const IndexLazyRoute = IndexLazyImport.update({
-  path: '/',
-  getParentRoute: () => rootRoute,
+    path: '/',
+    getParentRoute: () => rootRoute,
 } as any).lazy(() => import('./routes/index.lazy').then((d) => d.Route))
 
 // Populate the FileRoutesByPath interface
 
 declare module '@tanstack/react-router' {
-  interface FileRoutesByPath {
-    '/': {
-      id: '/'
-      path: '/'
-      fullPath: '/'
-      preLoaderRoute: typeof IndexLazyImport
-      parentRoute: typeof rootRoute
+    interface FileRoutesByPath {
+        '/': {
+            id: '/'
+            path: '/'
+            fullPath: '/'
+            preLoaderRoute: typeof IndexLazyImport
+            parentRoute: typeof rootRoute
+        }
+        '/about': {
+            id: '/about'
+            path: '/about'
+            fullPath: '/about'
+            preLoaderRoute: typeof AboutLazyImport
+            parentRoute: typeof rootRoute
+        }
+        '/banter': {
+            id: '/banter'
+            path: '/banter'
+            fullPath: '/banter'
+            preLoaderRoute: typeof BanterLazyImport
+            parentRoute: typeof rootRoute
+        }
     }
-    '/about': {
-      id: '/about'
-      path: '/about'
-      fullPath: '/about'
-      preLoaderRoute: typeof AboutLazyImport
-      parentRoute: typeof rootRoute
-    }
-    '/banter': {
-      id: '/banter'
-      path: '/banter'
-      fullPath: '/banter'
-      preLoaderRoute: typeof BanterLazyImport
-      parentRoute: typeof rootRoute
-    }
-  }
 }
 
 // Create and export the route tree
 
 export const routeTree = rootRoute.addChildren({
-  IndexLazyRoute,
-  AboutLazyRoute,
-  BanterLazyRoute,
+    IndexLazyRoute,
+    AboutLazyRoute,
+    BanterLazyRoute,
 })
 
 /* prettier-ignore-end */
