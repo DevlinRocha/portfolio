624136746b9a80a69565f65dada262f28b0e7e76
Devlin <devlincodes@gmail.com>
2024-07-03 01:33:10 -0700

    fix tsconfig path aliasing

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import Nav from '../components/Nav'
-import Footer from '../components/Footer'
+import Footer from '@/components/Footer'
 
 const TanStackRouterDevtools =
     process.env.NODE_ENV === 'production'
