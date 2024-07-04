59aabaaaeaf7ef7557410d890f9cccde5ce2dd85
Devlin <devlincodes@gmail.com>
2024-07-04 04:19:28 -0700

    redesign nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,23 +2,27 @@ import { Link } from '@tanstack/react-router'
 
 function Nav() {
     return (
-        <nav className="sticky top-0 z-10 bg-white">
-            <div className="flex gap-2 p-4">
-                <Link to="/" className="[&.active]:font-bold">
+        <nav className="3xl:mx-96 z-10 m-3 mx-8 flex h-16 items-end justify-between bg-white lg:mx-32 xl:mx-64 2xl:mx-80">
+            <Link to="/" className="px-5 py-2">
+                Devlin Rocha
+            </Link>
+
+            <div className="flex text-sm">
+                <Link to="/" className="px-5 py-2 [&.active]:font-bold">
                     home
-                </Link>{' '}
-                <Link to="/about" className="[&.active]:font-bold">
+                </Link>
+                <Link to="/about" className="px-5 py-2 [&.active]:font-bold">
                     about
                 </Link>
                 <a
                     href="https://drive.google.com/file/d/1DTqIS7jxcQzt12IpAFkbrl4z3anU3q5l/view?usp=sharing"
                     target="_blank"
                     rel="noReferrer"
+                    className="px-5 py-2"
                 >
                     résumé
                 </a>
             </div>
-            <hr />
         </nav>
     )
 }
