265fee85bd10b1c342ef84f7e7ddad282a3a390b
Devlin <devlincodes@gmail.com>
2024-07-04 15:25:17 -0700

    adjust spacing, font size, and copy

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -7,10 +7,21 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
     return (
-        <div className="3xl:mx-96 mx-8 flex flex-col gap-8 lg:mx-32 xl:mx-64 2xl:mx-80 2xl:gap-16">
-            <div className="flex flex-col text-pretty text-xl sm:text-2xl md:text-3xl lg:text-4xl">
-                <span>hey there! 👋 i&apos;m Devlin,</span>
-                <span>a software engineer in the San Francisco Bay Area</span>
+        <div className="3xl:mx-96 mx-8 mt-24 flex flex-col gap-16 lg:mx-32 lg:gap-32 xl:mx-64 2xl:mx-80">
+            <div className="flex flex-col gap-8 text-pretty text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-6xl">
+                <h1>hey, i&apos;m Devlin 👋</h1>
+
+                <h1>i code, design, and game</h1>
+
+                <p className="text-base">
+                    i'm a software engineer based in San Francisco, California
+                    with experience implementing robust and scalable web
+                    applications, improving development workflows, and
+                    delivering high-quality solutions aligned with business
+                    goals. i'm passionate about improving the lives of others
+                    through software and am constantly looking to learn new
+                    things
+                </p>
             </div>
 
             <div className="flex flex-col gap-8 2xl:gap-16">
