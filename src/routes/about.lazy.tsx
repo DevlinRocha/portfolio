0d5c1c1c7171b2427bfafb3650118a9800419911
Devlin <devlincodes@gmail.com>
2024-07-07 04:00:14 -0700

    adjust responsive spacing

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -6,7 +6,7 @@ export const Route = createLazyFileRoute('/about')({
 
 function About() {
     return (
-        <main className="mx-5 mt-8 flex flex-col gap-12 lg:mx-32 lg:mt-24 lg:gap-32 xl:mx-64 xl:gap-40 2xl:mx-80">
+        <main className="mx-5 mt-8 flex flex-col gap-16 lg:mx-32 lg:mt-24 xl:mx-64 2xl:mx-80">
             <div className="flex flex-col gap-2 xl:gap-8">
                 <h1 className="xs:text-5xl font-serif text-4xl font-semibold sm:text-6xl">
                     i&apos;m Devlin,
