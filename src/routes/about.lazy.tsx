d5b1f025010241b084968d703aebdffea6ddafa5
Devlin <devlincodes@gmail.com>
2024-07-07 03:44:28 -0700

    adjust text and layout spacing responsiveness

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -6,27 +6,32 @@ export const Route = createLazyFileRoute('/about')({
 
 function About() {
     return (
-        <main className="mx-8 mt-8 flex flex-col gap-8 lg:mx-32 lg:mt-24 xl:mx-64 2xl:mx-80">
-            <h1 className="font-serif text-xl font-semibold sm:text-2xl md:text-3xl lg:text-4xl xl:text-6xl">
-                i&apos;m Devlin,
-            </h1>
-            <p>
-                a music, gaming, and tech enthusiast with a passion for design,
-                creation, and collaboration!
-            </p>
-            <p>
-                my passion for technology is life-long, stemming from a
-                childhood full of video games. it wasn&apos;t long before i was
-                taking consoles apart, building computers, and diving into
-                programming
-            </p>
-            <p>
-                in addition to programming and video games, my hobbies include
-                music / audio production, video editing / content creation, and
-                hanging out with my dog, Charlie
-            </p>
+        <main className="mx-5 mt-8 flex flex-col gap-12 lg:mx-32 lg:mt-24 lg:gap-32 xl:mx-64 xl:gap-40 2xl:mx-80">
+            <div className="flex flex-col gap-2 xl:gap-8">
+                <h1 className="xs:text-5xl font-serif text-4xl font-semibold sm:text-6xl">
+                    i&apos;m Devlin,
+                </h1>
 
-            <div className="flex w-full select-none flex-col items-center gap-8 2xl:gap-16">
+                <div className="2xs:text-base 2xs:leading-7 mt-4 flex flex-col gap-8 text-pretty text-sm leading-7 sm:mt-8 xl:mt-0">
+                    <p>
+                        a music, gaming, and tech enthusiast with a passion for
+                        design, creation, and collaboration
+                    </p>
+                    <p>
+                        my passion for technology is life-long, stemming from a
+                        childhood full of video games. it wasn&apos;t long
+                        before i was taking consoles apart, building computers,
+                        and diving into programming
+                    </p>
+                    <p>
+                        in addition to programming and video games, my hobbies
+                        include music / audio production, video editing /
+                        content creation, and hanging out with my dog, Charlie
+                    </p>
+                </div>
+            </div>
+
+            <div className="2xs:gap-8 xs:gap-10 flex w-full select-none flex-col items-center gap-4 2xl:gap-16">
                 <img
                     src="about.png"
                     alt="me sitting with my laptop"
