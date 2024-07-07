8cf14c161a7e2bfeed5e372ff9ed8f09fe61c773
Devlin <devlincodes@gmail.com>
2024-07-07 03:41:57 -0700

    adjust text and layout spacing responsiveness

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -7,13 +7,16 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
     return (
-        <div className="mx-8 mt-8 flex flex-col gap-16 lg:mx-32 lg:mt-24 lg:gap-32 xl:mx-64 xl:gap-40 2xl:mx-80">
-            <div className="flex flex-col gap-2 text-pretty font-serif text-xl font-semibold sm:text-2xl md:text-3xl lg:text-4xl xl:gap-8 xl:text-6xl">
-                <h1>hey, i&apos;m Devlin 👋</h1>
+        <div className="mx-5 mt-8 flex flex-col gap-12 lg:mx-32 lg:mt-24 lg:gap-32 xl:mx-64 xl:gap-40 2xl:mx-80">
+            <div className="flex flex-col gap-2 xl:gap-8">
+                <h1 className="xs:text-5xl text-balance font-serif text-4xl font-semibold leading-snug sm:text-6xl">
+                    <span>hey, i&apos;m Devlin 👋</span>
+                    <span className="xs:mt-8 xs:leading-none mt-0.5 block leading-tight">
+                        i code, design, and game
+                    </span>
+                </h1>
 
-                <h1>i code, design, and game</h1>
-
-                <p className="mt-4 font-sans text-base font-normal xl:mt-0">
+                <p className="2xs:text-base 2xs:leading-7 mt-4 text-pretty text-sm leading-7 sm:mt-8 xl:mt-0">
                     i'm a software engineer based in San Francisco, California
                     with experience implementing robust and scalable web
                     applications. i'm passionate about improving the lives of
@@ -22,7 +25,7 @@ function Index() {
                 </p>
             </div>
 
-            <div className="flex select-none flex-col gap-8 2xl:gap-16">
+            <div className="2xs:gap-8 xs:gap-10 flex select-none flex-col gap-4 2xl:gap-16">
                 <Project name="Banter" />
                 <Project name="VVordle" />
                 <Project name="Pokémon Roulette" />
