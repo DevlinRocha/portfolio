9b12d177f8cffc3dc3bb7a8a308ab3bcc0707c0a
Devlin <devlincodes@gmail.com>
2024-07-08 01:07:54 -0700

    footer links to #home

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
     return (
         <footer className="h-64">
-            <div className="flex h-full flex-col items-center justify-center gap-4 p-4">
-                <div className="flex select-none gap-4 text-black/80">
+            <div className="flex h-full flex-col items-center justify-center gap-4 p-4 text-black/80">
+                <div className="flex select-none gap-4">
                     <a
                         href="https://twitter.com/DevlinRocha"
                         target="_blank"
@@ -29,9 +29,12 @@ function Footer() {
                     </a>
                 </div>
 
-                <span className="text-black/70">
+                <a
+                    href="/#home"
+                    className="transition-text hover:text-black hover:underline"
+                >
                     designed and developed by Devlin Rocha
-                </span>
+                </a>
             </div>
         </footer>
     )
