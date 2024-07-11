ba8212837f1060df9cb01dea75b8984b4dde7708
Devlin <devlincodes@gmail.com>
2024-07-11 15:56:42 -0700

    adjust `height`; remove unused <div> element

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,41 +1,39 @@
 function Footer() {
     return (
-        <footer className="h-64">
-            <div className="flex h-full flex-col items-center justify-center gap-4 p-4 text-black/80">
-                <div className="flex select-none gap-4">
-                    <a
-                        href="https://twitter.com/DevlinRocha"
-                        target="_blank"
-                        rel="noReferrer"
-                        className="transition-text hover:text-black hover:underline"
-                    >
-                        Twitter
-                    </a>
-                    <a
-                        href="https://github.com/DevlinRocha"
-                        target="_blank"
-                        rel="noReferrer"
-                        className="transition-text hover:text-black hover:underline"
-                    >
-                        GitHub
-                    </a>
-                    <a
-                        href="https://www.linkedin.com/in/DevlinRocha"
-                        target="_blank"
-                        rel="noReferrer"
-                        className="transition-text hover:text-black hover:underline"
-                    >
-                        LinkedIn
-                    </a>
-                </div>
-
+        <footer className="flex h-full flex-col items-center justify-center gap-4 p-4 text-black/80">
+            <div className="flex select-none gap-4">
                 <a
-                    href="/#home"
-                    className="text-balance text-center transition-text hover:text-black hover:underline"
+                    href="https://twitter.com/DevlinRocha"
+                    target="_blank"
+                    rel="noReferrer"
+                    className="transition-text hover:text-black hover:underline"
                 >
-                    designed and developed by Devlin Rocha
+                    Twitter
+                </a>
+                <a
+                    href="https://github.com/DevlinRocha"
+                    target="_blank"
+                    rel="noReferrer"
+                    className="transition-text hover:text-black hover:underline"
+                >
+                    GitHub
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/DevlinRocha"
+                    target="_blank"
+                    rel="noReferrer"
+                    className="transition-text hover:text-black hover:underline"
+                >
+                    LinkedIn
                 </a>
             </div>
+
+            <a
+                href="/#home"
+                className="text-balance text-center transition-text hover:text-black hover:underline"
+            >
+                designed and developed by Devlin Rocha
+            </a>
         </footer>
     )
 }
