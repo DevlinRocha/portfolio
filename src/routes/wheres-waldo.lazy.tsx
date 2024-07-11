888076ab3417a8a4bcbec5637b3041f4dfc1eb34
Devlin <devlincodes@gmail.com>
2024-07-11 15:57:50 -0700

    redesign project pages

--- src/routes/wheres-waldo.lazy.tsx
+++ src/routes/wheres-waldo.lazy.tsx
@@ -1,50 +1,42 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import Gallery from '@/components/Gallery'
 
 export const Route = createLazyFileRoute('/wheres-waldo')({
     component: wheresWaldo,
 })
 
+const galleryItems = [
+    {
+        src: 'wheres-waldo.png',
+        title: 'choose a level',
+        description: '6 different levels from the iconic book series',
+    },
+    {
+        src: 'wheres-waldo-guess.png',
+        title: 'find Waldo',
+        description: 'select anywhere in the picture to make a guess',
+    },
+    {
+        src: 'wheres-waldo-scores.png',
+        title: 'high scores',
+        description: 'find Waldo quick enough to make it on the leaderboard',
+    },
+    {
+        src: 'wheres-waldo-bottom.png',
+        title: 'more than just Waldo',
+        description: 'can you find all the different characters?',
+    },
+]
+
 function wheresWaldo() {
     return (
-        <div className="flex flex-col justify-center gap-8 sm:gap-16 lg:gap-24">
-            <img
-                src="wheres-waldo-hero.svg"
-                alt="Where's Waldo? hero image"
-                width={1920}
-                height={720}
-                className="drop-shadow-lg"
-            />
+        <div className="flex flex-col items-center">
+            <div className="w-full max-w-[2560px] bg-red-600 py-14 text-center">
+                <h1 className="font-display text-5xl">Where's Waldo?</h1>
+            </div>
 
-            <main className="flex flex-col gap-12 px-5 lg:px-32 xl:px-64 2xl:gap-16 2xl:px-80">
-                <div className="flex select-none flex-col text-sm 2xl:gap-1">
-                    <h1 className="xs:text-5xl font-display select-text text-4xl font-semibold leading-snug sm:text-6xl">
-                        Where's Waldo?
-                    </h1>
-
-                    <span className="text-black/80">
-                        {'{ '}
-                        <a
-                            href="https://github.com/DevlinRocha/wheres-waldo"
-                            target="_blank"
-                            rel="noReferrer"
-                            className="transition-text hover:text-black hover:underline"
-                        >
-                            source code
-                        </a>
-                        {' | '}
-                        <a
-                            href="https://devlinrocha.github.io/wheres-waldo/"
-                            target="_blank"
-                            rel="noReferrer"
-                            className="transition-text hover:text-black hover:underline"
-                        >
-                            deployed project
-                        </a>
-                        {' }'}
-                    </span>
-                </div>
-
-                <div className="2xs:text-base 2xs:leading-7 mb-8 flex flex-col gap-8 text-pretty text-sm leading-7 lg:mb-12">
+            <main className="flex h-full w-full flex-col items-center gap-8 pt-14">
+                <div className="flex w-[87.5lvw] max-w-prose flex-col gap-2 text-pretty text-sm leading-7 2xs:text-base 2xs:leading-7">
                     <p>
                         Where's Waldo? is a fullstack open source web version of
                         the iconic book series, built with <b>TypeScript</b>,{' '}
@@ -58,39 +50,27 @@ function wheresWaldo() {
                     </p>
                 </div>
 
-                <div className="2xs:gap-8 xs:gap-10 flex select-none flex-col gap-4 2xl:gap-16">
-                    <img
-                        src="wheres-waldo.png"
-                        alt="Where's Waldo? home screenshot"
-                        width={1920}
-                        height={1080}
-                        className="rounded-lg drop-shadow-lg"
-                    />
+                <div className="flex justify-center gap-2.5 text-sm md:text-base">
+                    <a
+                        href="https://devlinrocha.github.io/wheres-waldo/"
+                        target="_blank"
+                        rel="noReferrer"
+                        className="flex items-center rounded-full bg-black px-4 py-2 text-center text-white hover:bg-black/80 md:px-5 md:py-2.5"
+                    >
+                        view project
+                    </a>
 
-                    <img
-                        src="wheres-waldo-guess.png"
-                        alt="Where's Waldo? game screenshot"
-                        width={1920}
-                        height={1080}
-                        className="rounded-lg drop-shadow-lg"
-                    />
-
-                    <img
-                        src="wheres-waldo-scores.png"
-                        alt="Where's Waldo? high scores leaderboard screenshot"
-                        width={1920}
-                        height={1080}
-                        className="rounded-lg drop-shadow-lg"
-                    />
-
-                    <img
-                        src="wheres-waldo-bottom.png"
-                        alt="Where's Waldo? characters screenshot"
-                        width={1920}
-                        height={1080}
-                        className="rounded-lg drop-shadow-lg"
-                    />
+                    <a
+                        href="https://github.com/DevlinRocha/wheres-waldo"
+                        target="_blank"
+                        rel="noReferrer"
+                        className="flex items-center rounded-full border border-black/80 px-4 py-2 text-center text-black/80 hover:bg-black/80 hover:text-white md:px-5 md:py-2.5"
+                    >
+                        source code
+                    </a>
                 </div>
+
+                <Gallery items={galleryItems} />
             </main>
         </div>
     )
