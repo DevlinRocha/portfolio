8bf61676bfae71dc237b6b14066ce27e84ee48d4
Devlin <devlincodes@gmail.com>
2024-07-12 15:30:30 -0700

    adjust responsiveness for large screens

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -11,7 +11,7 @@ interface GalleryItem {
 function Gallery({ items }: GalleryProps) {
     return (
         <ul className="scrollbar-hide flex h-auto w-full snap-x snap-mandatory gap-5 overflow-x-auto py-4 md:gap-10 2xl:gap-20">
-            <div className="4xl:w-[32lvw] w-0 shrink-0 snap-align-none md:w-[4lvw] xl:w-[12lvw] 2xl:w-[26lvw]" />
+            <div className="4xl:w-[32lvw] 3xl:w-[25lvw] w-0 shrink-0 snap-align-none md:w-[4lvw] lg:w-[10lvw] xl:w-[16lvw] 2xl:w-[20lvw]" />
 
             {items.map(({ src, title, description }: GalleryItem, index) => {
                 return (
@@ -44,7 +44,7 @@ function Gallery({ items }: GalleryProps) {
                 )
             })}
 
-            <div className="4xl:w-[32lvw] w-0 shrink-0 snap-align-none md:w-[4lvw] xl:w-[12lvw] 2xl:w-[26lvw]" />
+            <div className="4xl:w-[32lvw] 3xl:w-[25lvw] w-0 shrink-0 snap-align-none md:w-[4lvw] lg:w-[10lvw] xl:w-[16lvw] 2xl:w-[20lvw]" />
         </ul>
     )
 }
