35bdbdd6d1ec676dea9194afd2a350b479d736c7
Devlin <devlincodes@gmail.com>
2024-07-12 00:00:38 -0700

    resolve base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), TanStackRouterVite()],
-    base: '/staging',
+    base: '/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
