689fa94ed8371798f165369895f48990e4743468
Devlin <devlincodes@gmail.com>
2024-07-15 16:32:29 -0700

    edit size and color

--- src/components/Caret.tsx
+++ src/components/Caret.tsx
@@ -6,25 +6,32 @@ interface CaretProps {
 function Caret({ onClick, className }: CaretProps) {
     return (
         <svg
-            width="10"
-            height="12"
-            viewBox="0 0 10 12"
+            width="5"
+            height="5"
+            viewBox="0 0 5 5"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             onClick={onClick}
             className={className}
         >
-            <path
-                d="M2 6L7 1"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
+            <rect
+                width="5"
+                height="5"
+                rx="2.5"
+                fill="#424245"
+                fill-opacity="0.72"
             />
             <path
-                d="M2 6L7 11"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
+                d="M2 2.5L3 1.5"
+                stroke="white"
+                stroke-width="0.5"
+                stroke-linecap="round"
+            />
+            <path
+                d="M2 2.5L3 3.5"
+                stroke="white"
+                stroke-width="0.5"
+                stroke-linecap="round"
             />
         </svg>
     )
