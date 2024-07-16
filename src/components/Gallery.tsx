571756fbfc645329a5dda90f14cd2a9243daaf11
Devlin <devlincodes@gmail.com>
2024-07-15 21:24:18 -0700

    change layout; add transitions

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react'
 import { Link } from '@tanstack/react-router'
 import Caret from './Caret'
 import { slug } from '@/utilities/functions'
@@ -13,63 +14,105 @@ interface GalleryItem {
 }
 
 function Gallery({ items }: GalleryProps) {
+    const captions = useRef<(HTMLElement | null)[]>([])
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        console.log(entries)
+                        entry.target.classList.remove('opacity-0')
+                    } else {
+                        entry.target.classList.add('opacity-0')
+                    }
+                })
+            },
+            {
+                threshold: 0.5,
+            }
+        )
+
+        captions.current.forEach((el) => {
+            if (el) observer.observe(el)
+        })
+
+        return () => {
+            captions.current.forEach((el) => {
+                if (el) observer.unobserve(el)
+            })
+        }
+    }, [])
+
     function handleClick(e: React.MouseEvent, direction: 'left' | 'right') {
         e.stopPropagation()
 
         const target = e.currentTarget as HTMLElement
-        target?.parentElement?.scrollBy(direction === 'left' ? -16 : 16, 0)
+
+        target?.parentElement?.previousElementSibling?.scrollBy(
+            direction === 'left' ? -16 : 16,
+            0
+        )
     }
 
     return (
-        <ul className="scrollbar-hide flex h-auto w-full snap-x snap-mandatory gap-5 overflow-x-auto scroll-smooth py-4 md:gap-10 2xl:gap-20">
-            <div className="w-0 shrink-0 snap-align-none md:w-[4lvw] lg:w-[10lvw] xl:w-[16lvw] 2xl:w-[20lvw] 3xl:w-[25lvw] 4xl:w-[32lvw]" />
+        <>
+            <ul className="scrollbar-hide flex h-auto w-full snap-x snap-mandatory gap-5 overflow-x-auto scroll-smooth">
+                {items.map(
+                    ({ src, title, description }: GalleryItem, index) => {
+                        return (
+                            <li
+                                id={`${slug(title)}`}
+                                key={index}
+                                className="m-0 shrink-0 snap-center snap-always first:md:ml-[4lvw] last:md:mr-[4lvw] first:lg:ml-[10lvw] last:lg:mr-[10lvw] first:xl:ml-[16lvw] last:xl:mr-[16lvw] first:2xl:ml-[20lvw] last:2xl:mr-[20lvw] first:3xl:ml-[25lvw] last:3xl:mr-[25lvw] first:4xl:ml-[32lvw] last:4xl:mr-[32lvw]"
+                            >
+                                <Link hash={`${slug(title)}`} draggable={false}>
+                                    <figure className="flex h-full flex-col items-center gap-10">
+                                        <div className="flex h-full flex-col items-center gap-5 rounded-2xl bg-neutral-300 p-5 md:p-12 lg:gap-8">
+                                            <img
+                                                src={src}
+                                                width={1920}
+                                                height={1080}
+                                                draggable={false}
+                                                className="max-h-[50vh] w-fit max-w-[95lvw] object-contain"
+                                            />
 
-            <Caret
-                onClick={(e) => handleClick(e, 'left')}
-                className="sticky left-0 top-1/4 z-10 min-h-14 min-w-14 cursor-pointer snap-align-none xs:left-4 xs:top-1/3 md:left-1/4 4xl:left-1/3"
-            />
+                                            <div
+                                                ref={(item) =>
+                                                    (captions.current[index] =
+                                                        item)
+                                                }
+                                                className="flex w-[87.5lvw] max-w-[297px] flex-col items-center gap-5 transition-opacity duration-500 ease-out 2xs:max-w-[360px] xs:max-w-[432px] sm:max-w-[504px] md:max-w-[648px] lg:gap-8"
+                                            >
+                                                <figcaption className="text-lg font-medium text-sky-500 underline underline-offset-8 md:text-xl lg:text-2xl">
+                                                    {title}
+                                                </figcaption>
 
-            {items.map(({ src, title, description }: GalleryItem, index) => {
-                return (
-                    <li
-                        id={`${slug(title)}`}
-                        key={index}
-                        className="shrink-0 snap-center snap-always"
-                    >
-                        <Link hash={`${slug(title)}`} draggable={false}>
-                            <figure className="flex flex-col items-center gap-10">
-                                <div className="flex justify-center rounded-2xl bg-neutral-400 p-5 md:p-12">
-                                    <img
-                                        src={src}
-                                        width={1920}
-                                        height={1080}
-                                        draggable={false}
-                                        className="max-h-[50vh] w-fit max-w-[95lvw] object-contain"
-                                    />
-                                </div>
+                                                <p className="max-w-prose text-pretty text-lg lg:text-3xl">
+                                                    {description}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </figure>
+                                </Link>
+                            </li>
+                        )
+                    }
+                )}
+            </ul>
 
-                                <div className="flex w-[87.5lvw] max-w-[297px] flex-col items-center gap-5 2xs:max-w-[360px] xs:max-w-[432px] sm:max-w-[504px] md:max-w-[648px] lg:gap-8">
-                                    <figcaption className="text-lg font-medium text-sky-500 underline underline-offset-8 md:text-xl lg:text-2xl">
-                                        {title}
-                                    </figcaption>
+            <div className="mb-4 flex gap-4">
+                <Caret
+                    onClick={(e) => handleClick(e, 'left')}
+                    className="min-h-14 min-w-14 cursor-pointer"
+                />
 
-                                    <p className="max-w-prose text-pretty text-lg lg:text-3xl">
-                                        {description}
-                                    </p>
-                                </div>
-                            </figure>
-                        </Link>
-                    </li>
-                )
-            })}
-
-            <Caret
-                onClick={(e) => handleClick(e, 'right')}
-                className="sticky right-0 top-1/4 z-10 min-h-14 min-w-14 -scale-x-100 cursor-pointer snap-align-none xs:right-4 xs:top-1/3 md:right-1/4 4xl:right-1/3"
-            />
-
-            <div className="w-0 shrink-0 snap-align-none md:w-[4lvw] lg:w-[10lvw] xl:w-[16lvw] 2xl:w-[20lvw] 3xl:w-[25lvw] 4xl:w-[32lvw]" />
-        </ul>
+                <Caret
+                    onClick={(e) => handleClick(e, 'right')}
+                    className="min-h-14 min-w-14 -scale-x-100 cursor-pointer"
+                />
+            </div>
+        </>
     )
 }
 
