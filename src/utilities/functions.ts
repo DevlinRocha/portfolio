8de974e3290749c0d4121bfd2c7dffb8da2b15ae
Devlin <devlincodes@gmail.com>
2024-07-29 18:26:20 -0700

    delete file (no longer used)

--- src/utilities/functions.ts
+++
@@ -1,8 +0,0 @@
-export function slug(string: string) {
-    return string
-        .normalize('NFKD')
-        .replace(/\p{Diacritic}/gu, '')
-        .replace(/[^\w\s]|_/g, '')
-        .replace(/\s+/g, '-')
-        .toLowerCase()
-}
