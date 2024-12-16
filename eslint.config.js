2708fb260afa8dc813931e6c598bb51fb8dc71fe
Devlin <devlin@devlinrocha.com>
2024-12-15 20:29:24 -0800

    include react version in eslint config settings

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,11 @@ import tseslint from 'typescript-eslint'
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
 
 export default [
+    {
+        settings: {
+            react: { version: 'detect' },
+        },
+    },
     { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
     {
         languageOptions: {
