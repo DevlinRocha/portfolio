d948149421313bc42e5dd54c005fc67787d2bd68
Devlin <devlin@devlinrocha.com>
2024-12-30 02:33:19 -0800

    add resources to script output

--- scripts/generateAssetList.js
+++ scripts/generateAssetList.js
@@ -3,12 +3,29 @@ import path from 'path'
 
 const distDir = path.resolve('dist')
 const manifestPath = path.join(distDir, '.vite/manifest.json')
-const outputPath = path.join(distDir, 'asset-list.js')
-
+const outputPath = path.join(distDir, 'resource-list.js')
 const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'))
-const assetUrls = Object.values(manifest).map((entry) => `/${entry.file}`)
-const assetListContent = `const ASSET_LIST = ${JSON.stringify(assetUrls, null, 2)};`
 
-fs.writeFileSync(outputPath, assetListContent)
+const routesToCache = [
+    '/',
+    '/about',
+    '/banter',
+    '/pokemon-roulette',
+    '/vvordle',
+    '/wheres-waldo',
+]
 
-console.log('Asset list generated:', outputPath)
+const assetPaths = Object.values(manifest).flatMap((entry) => [
+    `/${entry.file}`,
+    ...(entry.css ? [`/${entry.css}`] : []),
+])
+
+const favicons = ['icon.svg', 'icon-512.png', 'icon-192.png', '/favicon.ico']
+
+const resourcesToCache = [...routesToCache, ...assetPaths, ...favicons]
+
+const resourceList = `const RESOURCE_LIST = ${JSON.stringify(resourcesToCache, null, 2)};`
+
+fs.writeFileSync(outputPath, resourceList)
+
+console.log('Resource list generated:', outputPath)
