5096d1311ca9b66fec813209978becb0f8f2e5ea
Devlin <devlin@devlinrocha.com>
2024-12-29 22:58:46 -0800

    emit manifest during build step

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), TanStackRouterVite()],
+    build: {
+        manifest: true,
+    },
     base: '/',
     resolve: {
         alias: {
