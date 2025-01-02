bb33056e50eaa1d2fb53bfdf760d5807ddc28f66
Devlin <devlin@devlinrocha.com>
2025-01-01 22:00:14 -0800

    added `--ignore-errors` flag; added `excludedProjects` array; renamed variables

--- scripts/build.js
+++ scripts/build.js
@@ -1,18 +1,31 @@
 import { execSync } from 'child_process'
 import { existsSync, readdirSync } from 'fs'
-import { resolve, join } from 'path'
-import { argv } from 'process'
+import { join, resolve, basename } from 'path'
+import { argv, cwd, exit } from 'process'
 
-const folders = ['packages', 'apps']
+const directories = ['packages', 'apps']
 const args = argv.slice(2)
+const flags = args.filter((arg) => arg.startsWith('--'))
+const projects = args.filter((arg) => !flags.includes(arg))
+const ignoreErrors = flags.includes('--ignore-errors')
 
-function getProjects(folder) {
-    const dirPath = join(resolve(), folder)
+if (ignoreErrors) console.log('Ignoring errors...')
+
+const excludedProjects = ['packages/api'].map((project) =>
+    resolve(join(cwd(), project))
+)
+
+function getProjects(directory) {
+    const dirPath = join(resolve(), directory)
     if (!existsSync(dirPath)) return []
 
     return readdirSync(dirPath)
         .map((project) => join(dirPath, project))
-        .filter((projectPath) => existsSync(join(projectPath, 'package.json')))
+        .filter(
+            (projectPath) =>
+                existsSync(join(projectPath, 'package.json')) &&
+                !excludedProjects.includes(projectPath)
+        )
 }
 
 function buildProject(projectPath) {
@@ -25,27 +38,30 @@ function buildProject(projectPath) {
         console.log(`✅ Successfully built ${projectPath}`)
     } catch (error) {
         console.error(`❌ Error building ${projectPath}: ${error.message}`)
+        if (!ignoreErrors) {
+            exit(1)
+        }
     }
 }
 
-if (args.length === 0) {
-    folders.flatMap(getProjects).forEach(buildProject)
+if (projects.length === 0) {
+    directories.flatMap(getProjects).forEach(buildProject)
 } else {
-    const allProjects = folders.flatMap(getProjects)
+    const projectPaths = directories.flatMap(getProjects)
 
     const projectMap = Object.fromEntries(
-        allProjects.map((projectPath) => [
-            projectPath.split('/').pop(),
-            projectPath,
-        ])
+        projectPaths.map((projectPath) => [basename(projectPath), projectPath])
     )
 
-    args.forEach((name) => {
+    projects.forEach((name) => {
         const projectPath = projectMap[name]
         if (projectPath) {
             buildProject(projectPath)
         } else {
             console.error(`Project '${name}' not found.`)
+            if (!ignoreErrors) {
+                exit(1)
+            }
         }
     })
 }
