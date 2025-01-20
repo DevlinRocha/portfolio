2e693aa3332e96a99b283cd6d224b0907f86d2ff
Devlin <devlin@devlinrocha.com>
2025-01-20 01:28:24 -0800

    add initial mobile responsiveness

--- apps/frontend/src/routes/blog.index.lazy.tsx
+++ apps/frontend/src/routes/blog.index.lazy.tsx
@@ -30,11 +30,11 @@ function Blog() {
                 <h1 className="font-display text-5xl text-white">Blog</h1>
             </header>
 
-            <ul className="flex w-[87.5lvw] flex-wrap gap-8">
+            <ul className="flex w-[87.5lvw] flex-col flex-wrap gap-8 md:flex-row">
                 {data.slice(0).map((post) => {
                     return (
                         <li
-                            className="group h-[481px] w-[calc(50%-32px)] overflow-clip rounded-2xl bg-white"
+                            className="group overflow-clip rounded-2xl bg-white md:h-[481px] md:w-[calc(50%-32px)]"
                             key={post.id}
                         >
                             <Link
@@ -49,11 +49,11 @@ function Blog() {
                                     draggable={false}
                                     // alt={post.image.alt}
                                     onLoad={handleLoad}
-                                    className={`group-hover:scale-101 h-[266px] rounded-t-2xl bg-neutral-600 object-contain transition-transform duration-500 ${loading ? 'animate-loading' : 'animate-none'}`}
+                                    className={`group-hover:scale-101 h-40 rounded-t-2xl bg-neutral-600 object-contain transition-transform duration-500 md:h-[266px] ${loading ? 'animate-loading' : 'animate-none'}`}
                                 />
 
-                                <div className="flex h-full w-full select-none flex-col gap-4 p-8 text-gray-500">
-                                    <div className="flex gap-2">
+                                <div className="flex h-full w-full select-none flex-col gap-4 p-6 text-xs font-bold text-gray-500 md:p-8">
+                                    <div className="flex gap-1 md:gap-2">
                                         {Array.isArray(post.categories) &&
                                             post.categories.map(
                                                 (category, index) => (
@@ -69,11 +69,11 @@ function Blog() {
                                             ))}
                                     </div>
 
-                                    <h2 className="font-serif text-lg font-bold text-black md:text-xl lg:text-2xl">
+                                    <h2 className="font-serif text-xl leading-6 text-black md:text-xl lg:text-2xl">
                                         {post.title}
                                     </h2>
 
-                                    <span className="flex h-full items-end">
+                                    <span className="flex h-full items-end text-sm font-semibold leading-[1.125rem]">
                                         {new Date(
                                             post.created_at
                                         ).toLocaleDateString(undefined, {
