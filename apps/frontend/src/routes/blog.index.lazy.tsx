99cfe4bf90fbe6f0e145a4eab8c06d055f9faafd
Devlin <devlin@devlinrocha.com>
2025-02-15 08:15:02 -0800

    use `<BlogNav />`

--- apps/frontend/src/routes/blog.index.lazy.tsx
+++ apps/frontend/src/routes/blog.index.lazy.tsx
@@ -1,6 +1,7 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import { trpc } from '@/api/trpcClient'
 import BlogCard from '@/components/BlogCard'
+import BlogNav from '@/components/BlogNav'
 import DefaultNotFound from '@/components/DefaultNotFound'
 
 export const Route = createLazyFileRoute('/blog/')({
@@ -19,66 +20,75 @@ function Blog() {
     const firstPost = data[0]
 
     return (
-        <main
-            itemScope
-            itemType="https://schema.org/Blog"
-            className="flex h-full w-full flex-col items-center gap-4 bg-neutral-100 pb-24 md:pb-32 lg:gap-6 lg:pb-36"
-        >
-            <header className="mt-8 w-[87.5lvw] max-w-[992px] text-left md:mt-10 lg:mt-[52px]">
-                <h1
-                    className="font-display text-2xl font-bold md:text-3xl lg:text-4xl"
-                    itemProp="headline"
-                >
-                    Blog
-                </h1>
-            </header>
-
-            <ul
-                itemProp="mainEntity"
-                className="flex w-[87.5lvw] max-w-[992px] flex-col flex-wrap gap-8 sm:flex-row"
+        <>
+            <BlogNav />
+            <main
+                itemScope
+                itemType="https://schema.org/Blog"
+                className="flex h-full w-full flex-col items-center gap-4 bg-neutral-100 pb-24 md:pb-32 lg:gap-6 lg:pb-36"
             >
-                <li
-                    itemScope
-                    itemType="https://schema.org/BlogPosting"
-                    className="group overflow-clip rounded-2xl bg-white lg:max-h-[538px]"
-                    key={firstPost.id}
-                >
-                    <BlogCard
-                        id={firstPost.id}
-                        title={firstPost.title}
-                        image={firstPost.image}
-                        categories={firstPost.categories}
-                        tags={firstPost.tags}
-                        created_at={firstPost.created_at}
-                        updated_at={
-                            firstPost.updated_at || firstPost.created_at
-                        }
-                        created_at_formatted={firstPost.created_at_formatted}
-                        featured
-                    />
-                </li>
+                <header className="mt-8 w-[87.5lvw] max-w-[992px] text-left md:mt-10 lg:mt-[52px]">
+                    <h1
+                        className="font-display text-2xl font-bold md:text-3xl lg:text-4xl"
+                        itemProp="headline"
+                    >
+                        Latest Posts
+                    </h1>
+                </header>
 
-                {data.slice(1).map((post) => {
-                    return (
-                        <li
-                            itemType="https://schema.org/BlogPosting"
-                            className="group overflow-clip rounded-2xl bg-white md:w-[calc(50%-16px)] lg:max-h-[538px]"
-                            key={post.id}
-                        >
-                            <BlogCard
-                                id={post.id}
-                                title={post.title}
-                                image={post.image}
-                                categories={post.categories}
-                                tags={post.tags}
-                                created_at={post.created_at}
-                                updated_at={post.updated_at || post.created_at}
-                                created_at_formatted={post.created_at_formatted}
-                            />
-                        </li>
-                    )
-                })}
-            </ul>
-        </main>
+                <ul
+                    itemProp="mainEntity"
+                    className="flex w-[87.5lvw] max-w-[992px] flex-col flex-wrap gap-8 sm:flex-row"
+                >
+                    <li
+                        itemScope
+                        itemType="https://schema.org/BlogPosting"
+                        className="group overflow-clip rounded-2xl bg-white lg:max-h-[538px]"
+                        key={firstPost.id}
+                    >
+                        <BlogCard
+                            id={firstPost.id}
+                            title={firstPost.title}
+                            image={firstPost.image}
+                            categories={firstPost.categories}
+                            tags={firstPost.tags}
+                            created_at={firstPost.created_at}
+                            updated_at={
+                                firstPost.updated_at || firstPost.created_at
+                            }
+                            created_at_formatted={
+                                firstPost.created_at_formatted
+                            }
+                            featured
+                        />
+                    </li>
+
+                    {data.slice(1).map((post) => {
+                        return (
+                            <li
+                                itemType="https://schema.org/BlogPosting"
+                                className="group overflow-clip rounded-2xl bg-white md:w-[calc(50%-16px)] lg:max-h-[538px]"
+                                key={post.id}
+                            >
+                                <BlogCard
+                                    id={post.id}
+                                    title={post.title}
+                                    image={post.image}
+                                    categories={post.categories}
+                                    tags={post.tags}
+                                    created_at={post.created_at}
+                                    updated_at={
+                                        post.updated_at || post.created_at
+                                    }
+                                    created_at_formatted={
+                                        post.created_at_formatted
+                                    }
+                                />
+                            </li>
+                        )
+                    })}
+                </ul>
+            </main>
+        </>
     )
 }
